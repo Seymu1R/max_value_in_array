d0c74fae220a17e12b1bd25692002d7f1c806427
Seymu1R <102611811+Seymu1R@users.noreply.github.com>
2022-04-19 16:30:14 +0400

    first commit

---
+++ Max_value_of_array/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Bigest_Array_s_index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int C = 0;
+            int maxValue = 0;
+            int[] arr = { 999, 8, 49, 17, 5 ,102,33,895,3};
+
+            for (int i = arr.Length - 1; i > -1; i--)
+            {
+                C = arr[i];
+
+                if (C > maxValue)
+                {
+                    maxValue = C;
+                }
+                
+            }
+            Console.WriteLine(maxValue);
+        }
+    }
+}
